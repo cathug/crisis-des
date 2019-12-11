a0376f79a9cf3cf39cb7988405bbb2cf52b82ccb
cathug <16094449+cathug@users.noreply.github.com>
2019-12-12 00:29:32 +0800

    wip queue_simulation.py

--- queue_simulation.py
+++ queue_simulation.py
@@ -7,7 +7,7 @@ NUM_HELPSEEKERS_IN_QUEUE = 0
 NUM_HELPSEEKERS_IN_PRIORITY_QUEUE = [0, 0, 0]
 MEAN_QUEUE_TIME_BEFORE_RENEGING = 7.0 # specify this as a float
 LAMBDA_RENEGE = 1.0/MEAN_QUEUE_TIME_BEFORE_RENEGING # mean reneging counts
-
+LUNCH_BREAK = 60 # 60 minute lunch break
 
 
 
@@ -16,44 +16,55 @@ class Counsellor:
         Counsellor Class with a limited number of counsellors to serve helpseekers
     '''
 
-    def __init__(self, env, counsellor_id, service_duration, lunch_duration):
+    def __init__(self, 
+                 env,
+                 counsellor_id,
+                 service_duration,
+                 lunch_duration=LUNCH_BREAK):
         self.env = env
-        self.state = env.process(self.work() )
-        self.counsellor_id = counsellor_id
+        self.state = env.process(self.shift() )
+        self.counsellor = f'Counsellor {counsellor_id}'
         self.service_duration = service_duration
         self.lunch_duration = lunch_duration
 
     #---------------------------------------------------------------------------
     
-    def work(self):
+    def shift(self):
         while True:
             
-            # start counselling
-            print(f'Counsellor {self.counsellor_id} starts working at {self.env.now}')
+            # start counselling for the first 6 hours
+            print(f'{self.counsellor} starts shift at {self.env.now}')
             yield self.env.timeout(self.service_duration)
 
 
             # get lunch break
-            print(f'Counsellor {self.counsellor_id} takes a lunch break')
-            self.env.process(self.lunch() )
+            yield self.env.process(self.lunch_break() )
 
-            # print(f'Counsellor {self.counsellor_id} takes a lunch break')
-            # try:
-            #     yield self.env.process(self.lunch() )
-            # except simpy.Interrupt:
-            #     # stop eating lunch 
-            #     print(f'Too many people waiting.'
-            #         f' Duty Officer summons counsellor'
-            #         f' {self.counsellor_id} back to work')
+            print(f'Counsellor returns from lunch at {self.env.now}')
+            yield self.env.timeout(self.service_duration)
+            
+            print(f'{self.counsellor} shift ends at {self.env.now}')
 
     
     #---------------------------------------------------------------------------
 
-    def lunch(self):
+    def lunch_break(self):
         '''
-            Lunch Break
+            Give counsellors a lunch break
+            but interrupt when there is an urgent case incoming
         '''
+
+        print(f'{self.counsellor} takes a lunch break at {self.env.now}')
+        
         yield self.env.timeout(self.lunch_duration)
+        print(f'{self.counsellor} lunch break ends at {self.env.now}')
+        
+        # try:
+        #     yield self.env.timeout(self.lunch_duration)
+        #     print(f'{self.counsellor} lunch break ends at {self.env.now}')
+        # except simpy.Interrupt as i:
+        #     print(f'{self.counsellor} lunch break interrupted at '
+        #         f'{self.env.now} due to {i.cause}')
 
 
 #-------------------------------------------------------end of Counsellors class
@@ -63,25 +74,24 @@ class Helpseeker:
         Helpseeker Class to be created according to an exponential distribution
     '''
 
-    def __init(self, env, helpseeker_id, openup_service, chat_duration):
+    def __init__(self, env, helpseeker_id, openup_service, chat_duration):
         self.env = env
         self.chat_duration = chat_duration
         self.openup_service = openup_service
-        self.helpseeker_id = f'Helpseeker {helpseeker_id}'
+        self.user = f'Helpseeker {helpseeker_id}'
 
 
     def accept_TOS(self):
-        # helpseeker patience follows an exponential distribution
-        patience = random.expovariate(LAMBDA_RENEGE)
+        
 
-        print(f'{self.helpseeker_id} has accepted TOS.  Chat session created')
+        print(f'{self.user} has accepted TOS.  Chat session created')
 
         with openup_service.request() as req:
             yield req
 
-            print(f'{self.helpseeker_id} now being served')
+            print(f'{self.user} now being served')
             yield self.env.timeout(self.chat_duration)
-            print(f'{self.helpseeker_id} chat session terminates')
+            print(f'{self.user} chat session terminates')
 
     def renege(self):
 
@@ -92,6 +102,9 @@ def main():
     env = simpy.Environment()
     counsellor = Counsellor(env, 1, 8, 1)
     env.run(until=20)
+    
+    # helpseeker patience follows an exponential distribution
+    patience = random.expovariate(LAMBDA_RENEGE)
 
 
 if __name__ == '__main__':
