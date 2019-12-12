40253794a6a038bbd8e627ed288d1b4ca030a810
cathug <16094449+cathug@users.noreply.github.com>
2019-12-13 00:22:04 +0800

    fixed queue simulation timestamps

--- queue_simulation.py
+++ queue_simulation.py
@@ -1,8 +1,11 @@
 '''
-    This program simulates the OpenUp Counselling Service platform and
-    helpseeker arrivals.  
+    This program uses Simpy to simulate the OpenUp Counselling Service 
+    platform and helpseeker arrivals.  
 
     Helpseekers will renege when they loose patience waiting in the queue
+
+    For more details about the Simpy syntax, please visit
+    https://simpy.readthedocs.io/en/latest/contents.html
 '''
 
 import simpy, random
@@ -19,14 +22,22 @@ MEAN_QUEUE_TIME_BEFORE_RENEGING = 7.0 # specify this as a float
 LAMBDA_RENEGE = 1.0/MEAN_QUEUE_TIME_BEFORE_RENEGING # mean reneging counts
 LUNCH_BREAK = 60 # 60 minute lunch break
 MEAN_CHAT_DURATION = 60 # average chat no longer than 60 minutes
-SIMULATION_DURATION = 180 #1440
+SIMULATION_DURATION = 1440
 MEAN_INTERARRIVAL_TIME = 7.0 # mean time between helpseekers arriving
 NUM_COUNSELLORS = 16
 SEED = 728
 
+RISKLEVEL_WEIGHTS = {
+    'CRISIS':   .05,
+    'HIGH':     .015,
+    'MEDIUM':   .16,
+    'LOW':      .82,
+} # Distribution of LOW/MEDIUM/HIGH/CRISIS - 82%/16%/1.5%/0.5%
+
+
 
 ################################################################################
-# drawing arrival times from distribitions
+# Drawing arrival times from distribitions
 ################################################################################
 
 def get_interarrival_time(mean_interarrival_time):
@@ -55,69 +66,63 @@ def get_chat_duration(mean_chat_duration):
 
 def get_priority():
     '''
-        Helpseeker priority getter
-
-        Distribution of 
-
+        Helpseeker priority getter - chooses a priority with replacement
     '''
 
-# class Counsellor:
-#     '''
-#         Counsellor Class with a limited number of counsellors to serve helpseekers
-#     '''
+    return random.choices(
+        list(RISKLEVEL_WEIGHTS.keys()), list(RISKLEVEL_WEIGHTS.values() ) )[0]
 
-#     def __init__(self, 
-#                  env,
-#                  counsellor_id,
-#                  service_duration,
-#                  lunch_duration=LUNCH_BREAK):
-#         self.env = env
-#         self.process = env.process(self.shift() )
-#         self.counsellor = f'Counsellor {counsellor_id}'
-#         self.service_duration = service_duration
-#         self.lunch_duration = lunch_duration
+################################################################################
+# Classes
+################################################################################
 
-#     #---------------------------------------------------------------------------
+class Counsellor:
+    '''
+        Counsellor Class with a limited number of counsellors to serve helpseekers
+    '''
+
+    def __init__(self, 
+                 env,
+                 counsellor_id,
+                 service_duration,
+                 lunch_duration=LUNCH_BREAK):
+        self.env = env
+        self.process = env.process(self.shift() )
+        self.counsellor = f'Counsellor {counsellor_id}'
+        self.service_duration = service_duration
+        self.lunch_duration = lunch_duration
+
+    #---------------------------------------------------------------------------
     
-#     def shift(self):
-#         while True:
-            
-#             # start counselling for the first 6 hours
-#             print(f'{self.counsellor} starts shift at {self.env.now}')
-#             yield self.env.timeout(self.service_duration)
+    def shift(self):
+        '''
+            Define a shift
+        '''
 
+        while True:
+            # start counselling for the first 6 hours
+            print(f'{self.counsellor} starts shift at {self.env.now}')
+            yield self.env.timeout(self.service_duration)
 
-#             # get lunch break
-#             yield self.env.process(self.lunch_break() )
-
-#             print(f'Counsellor returns from lunch at {self.env.now}')
-#             yield self.env.timeout(self.service_duration)
-            
-#             print(f'{self.counsellor} shift ends at {self.env.now}')
+            # get lunch break
+            yield self.env.process(self.lunch_break() )
+            yield self.env.timeout(self.service_duration)
+            print(f'{self.counsellor} shift ends at {self.env.now}')
 
     
-#     #---------------------------------------------------------------------------
+    #---------------------------------------------------------------------------
 
-#     def lunch_break(self):
-#         '''
-#             Give counsellors a lunch break
-#             but interrupt when there is an urgent case
-#         '''
+    def lunch_break(self):
+        '''
+            Give counsellors a lunch break
+            but interrupt when there is an urgent case
+        '''
 
-#         print(f'{self.counsellor} takes a lunch break at {self.env.now}')
-        
-#         yield self.env.timeout(self.lunch_duration)
-#         print(f'{self.counsellor} lunch break ends at {self.env.now}')
-        
-#         # try:
-#         #     yield self.env.timeout(self.lunch_duration)
-#         #     print(f'{self.counsellor} lunch break ends at {self.env.now}')
-#         # except simpy.Interrupt as i:
-#         #     print(f'{self.counsellor} lunch break interrupted at '
-#         #         f'{self.env.now} due to {i.cause}')
+        print(f'{self.counsellor} takes a lunch break at {self.env.now}')
+        yield self.env.timeout(self.lunch_duration)
+        print(f'{self.counsellor} lunch break ends at {self.env.now}')
 
-
-# #-------------------------------------------------------end of Counsellors class
+#-------------------------------------------------------end of Counsellors class
 
 class Helpseeker:
     '''
@@ -141,7 +146,7 @@ class Helpseeker:
         '''
 
         self.env = env
-        self.arrival_time = self.env.now
+        self.arrival_time = None
         self.openup_service = openup_service
         self.user = f'Helpseeker {helpseeker_id}'
         self.mean_chat_duration = mean_chat_duration
@@ -161,32 +166,37 @@ class Helpseeker:
         interarrival_time = get_interarrival_time(self.mean_interarrival_time)
         yield self.env.timeout(interarrival_time)
 
-
-        print(f'\n{self.user} has accepted TOS.  '
+        self.arrival_time = self.env.now
+        print(f'{self.user} has accepted TOS.  '
             f'Chat session created at t = {self.env.now}.')
+
 
         with self.openup_service.request() as req:
             # helpseeker patience follows an exponential distribution
             # wait for counsellor or renege
             patience = random.expovariate(LAMBDA_RENEGE)
             results = yield req | self.env.timeout(patience)
-
-            queue_time = self.env.now - self.arrival_time
             
             if req in results:
                 # counsellor now serving helpseeker
-                print(f'\n{self.user} now being served.')
+                time_now = self.env.now
+                queue_time = time_now - self.arrival_time
+                print(f'{self.user} now being served at {time_now}.  '
+                    f'User spent {queue_time} in the queue.')
+
                 chat_duration = get_chat_duration(self.mean_chat_duration)
                 yield self.env.timeout(chat_duration)
-                print(f'\n{self.user} chat session terminated successfully at t ='
-                    f' {self.env.now}.')
+                print(f'{self.user} chat session terminated successfully at t ='
+                    f' {self.env.now}. Chat lasted {chat_duration}')
 
             else:
                 # helpseeker reneged
-                print(f'\n{self.user} reneged after '
-                    f'spending t = {self.env.now} in the queue.')
+                print(f'{self.user} reneged after '
+                    f'spending t = {patience} in the queue.')
 
-    #---------------------------------------------------------------------------
+################################################################################
+# Main Function
+################################################################################
 
 def main():
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
@@ -204,12 +214,13 @@ def main():
     start_time = env.now
     duration = 0
 
-    for i in range(1, 10):
+    for i in range(1, 120):
         helpseekers.append(Helpseeker(env, i, counsellor) )
 
-    print(f'Total number of helpseekers created {len(helpseekers)}\n\n')
+    print(f'Total number of helpseekers created: {len(helpseekers)}\n\n')
 
     env.run(until=SIMULATION_DURATION) # daily simulation at the queue
+
 
 if __name__ == '__main__':
     main()
